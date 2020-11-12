6af036c3bc60de12ac9822928f3099aef38fbe3f
Jeysson Guevara <guevara@civicplus.com>
2020-11-12 09:31:18 -0500

    Added ship concept game

---
+++ Ship/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1605191386,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/ship-sheet0.png",
+		"images/tiledbackground.png",
+		"images/enemy-sheet0.png",
+		"images/bullet-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
